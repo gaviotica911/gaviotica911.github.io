2baf7a668979271464c7fa7cf0407cab785a0751
Gabriela <g.garcias2@uniandes.edu.co>
2025-09-17 08:24:14 -0500

    cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,8 @@ const Hero = () => {
             Empowering Progress Through Data and Technology
           </h2>
           <div className="flex items-center gap-3">
-            <ButtonPrimary label="Download CV" icon="download" />{" "}
+            <ButtonPrimary label="Download CV" icon="download" />
+            {"/images/cv_Gabriela_García_Suárez.pdf "}
             <ButtonOutline
               href="#about"
               label="Scroll down"
